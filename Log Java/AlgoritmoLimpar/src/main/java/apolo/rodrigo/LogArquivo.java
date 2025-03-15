d49fbaf62939deea9062bb27a34cb0f6a4b50ccd
orodrigoapolo <rodrigo.gsantos@sptech.school>
2025-03-15 09:12:17 -0300

    arrundo log

--- Log Java/AlgoritmoLimpar/src/main/java/apolo/rodrigo/LogArquivo.java
+++ Log Java/AlgoritmoLimpar/src/main/java/apolo/rodrigo/LogArquivo.java
@@ -1,4 +1,41 @@
 package apolo.rodrigo;
 
+import java.time.LocalDateTime;
+
 public class LogArquivo {
+
+    void leituraArquivo(){
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime dataArquivo = LocalDateTime.of(2024,3,15, 10, 0);
+
+        System.out.println("Iniciando o programa: " + now);
+        System.out.println("Abrindo o arquivo 'baseDeDados.excel'...");
+        System.out.println("Arquivo excel do dia: " + dataArquivo);
+    }
+
+    void lendoDado(){
+        System.out.println("Lendo o conteúdo do arquivo...");
+        System.out.println("leitura concluída!");
+        System.out.println();
+    }
+
+    void limpadoDados(){
+        System.out.println("Limpando o conteúdo do arquivo...");
+        System.out.println("Conteúdo limpo!");
+        System.out.println();
+    }
+
+    void salvadoDados(){
+        System.out.println("Conectando ao banco de dados...");
+        System.out.println("Salvando o conteúdo limpo no banco de dados...");
+        System.out.println("Dados salvos com sucesso!");
+        System.out.println();
+    }
+
+    void fechadoArquivoBanco(){
+        System.out.println("Fechando conexão da base de dados...");
+        System.out.println("Fechando o arquivo 'baseDeDados.excel'...");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("Programa finalizado em " + now);
+    }
 }
--- Log Java/AlgoritmoLimpar/src/main/java/apolo/rodrigo/Main.java
+++ Log Java/AlgoritmoLimpar/src/main/java/apolo/rodrigo/Main.java
@@ -4,35 +4,12 @@ import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
+        LogArquivo log = new LogArquivo();
 
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime dataArquivo = LocalDateTime.of(2024,3,15, 10, 0);
-
-        System.out.println("Iniciando o programa: " + now);
-        System.out.println("Abrindo o arquivo 'baseDeDados.excel'...");
-        System.out.println("Arquivo excel do dia: " + dataArquivo);
-
-        System.out.println("Lendo o conteúdo do arquivo...");
-        System.out.println("leitura concluída!");
-        System.out.println();
-
-        // Simulação da limpeza do conteúdo
-        System.out.println("Limpando o conteúdo do arquivo...");
-        System.out.println("Conteúdo limpo!");
-        System.out.println();
-
-
-        // Simulação do salvamento no banco de dados
-        System.out.println("Conectando ao banco de dados...");
-        System.out.println("Salvando o conteúdo limpo no banco de dados...");
-        System.out.println("Dados salvos com sucesso!");
-        System.out.println();
-
-
-        // Simulação do fechamento do arquivo
-        System.out.println("Fechando conexão da base de dados...");
-        System.out.println("Fechando o arquivo 'baseDeDados.excel'...");
-        now = LocalDateTime.now();
-        System.out.println("Programa finalizado em " + now);
+        log.leituraArquivo();
+        log.lendoDado();
+        log.limpadoDados();
+        log.salvadoDados();
+        log.fechadoArquivoBanco();
     }
 }
