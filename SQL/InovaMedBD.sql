760ee8b0bd46f51ae7d7c37e192090a78d06f61f
joaoschettini <joao.ssantos@sptech.school>
2025-04-27 21:35:16 -0300

    ajuste nomes tabela sql

--- SQL/InovaMedBD.sql
+++ SQL/InovaMedBD.sql
@@ -9,9 +9,9 @@ USE inovamed;
 -- -----------------------------------------------------
 -- Table Estado
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS Estado ;
+DROP TABLE IF EXISTS estado ;
 
-CREATE TABLE IF NOT EXISTS Estado (
+CREATE TABLE IF NOT EXISTS estado (
   idEstado INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(100) NULL,
   UF  VARCHAR(2) NULL,
@@ -21,24 +21,24 @@ CREATE TABLE IF NOT EXISTS Estado (
 -- -----------------------------------------------------
 -- Table Municipio
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS Municipio ;
+DROP TABLE IF EXISTS municipio ;
 
-CREATE TABLE IF NOT EXISTS Municipio (
+CREATE TABLE IF NOT EXISTS municipio (
   idMunicipio INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(200) NULL,
   cod INT,
   qtdPopulacao INT NULL,
   fkEstado INT NOT NULL,
-  FOREIGN KEY (fkEstado) REFERENCES Estado (idEstado)
+  FOREIGN KEY (fkEstado) REFERENCES estado (idEstado)
 );
 
 
 -- -----------------------------------------------------
 -- Table Usuario
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS Usuario ;
+DROP TABLE IF EXISTS usuario ;
 
-CREATE TABLE IF NOT EXISTS Usuario (
+CREATE TABLE IF NOT EXISTS usuario (
   idUsuario INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(255),
   CPF CHAR(11),
@@ -51,16 +51,16 @@ CREATE TABLE IF NOT EXISTS Usuario (
   dtInativo DATETIME NULL,
   fkEstado INT NULL,
   fkMunicipio INT NULL,
-  FOREIGN KEY (fkEstado) REFERENCES Estado (idEstado),
-  FOREIGN KEY (fkMunicipio) REFERENCES Municipio (idMunicipio)
+  FOREIGN KEY (fkEstado) REFERENCES estado (idEstado),
+  FOREIGN KEY (fkMunicipio) REFERENCES municipio (idMunicipio)
 );
 
 -- -----------------------------------------------------
 -- Table Estoque
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS Estoque ;
+DROP TABLE IF EXISTS estoque ;
 
-CREATE TABLE IF NOT EXISTS Estoque (
+CREATE TABLE IF NOT EXISTS estoque (
   idEstoque INT PRIMARY KEY AUTO_INCREMENT,
   nomeFarmaco VARCHAR(255) NOT NULL,
   qtdFarmaco INT NULL,
@@ -69,15 +69,15 @@ CREATE TABLE IF NOT EXISTS Estoque (
   lote VARCHAR(100) NULL,
   CATMAT VARCHAR(100) NULL,
   fkMunicipio INT NOT NULL,
-  FOREIGN KEY (fkMunicipio) REFERENCES Municipio (idMunicipio)
+  FOREIGN KEY (fkMunicipio) REFERENCES municipio (idMunicipio)
 );
 
 -- -----------------------------------------------------
 -- Table Alerta
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS Alerta ;
+DROP TABLE IF EXISTS alerta ;
 
-CREATE TABLE IF NOT EXISTS Alerta (
+CREATE TABLE IF NOT EXISTS alerta (
   idAlerta INT NOT NULL,
   fkEstoque INT NOT NULL,
   fkUsuario INT NOT NULL,
@@ -86,43 +86,43 @@ CREATE TABLE IF NOT EXISTS Alerta (
   dtAlerta DATETIME NULL,
   titulo VARCHAR(100) NULL,
   constraint pkCompostaAlerta PRIMARY KEY (idAlerta, fkEstoque, fkUsuario),
-  FOREIGN KEY (fkEstoque) REFERENCES Estoque (idEstoque),
-  FOREIGN KEY (fkUsuario) REFERENCES Usuario (idUsuario)
+  FOREIGN KEY (fkEstoque) REFERENCES estoque (idEstoque),
+  FOREIGN KEY (fkUsuario) REFERENCES usuario (idUsuario)
 );
 
 -- -----------------------------------------------------
 -- Table Marcador
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS Marcador ;
+DROP TABLE IF EXISTS marcador ;
 
-CREATE TABLE IF NOT EXISTS Marcador (
+CREATE TABLE IF NOT EXISTS marcador (
   idMarcador INT PRIMARY KEY AUTO_INCREMENT,
   max DECIMAL(10,2) NULL,
   min DECIMAL(10,2) NULL,
   antesMax INT NULL,
   antesMin INT NULL,
   fkUsuario INT NOT NULL,
-  FOREIGN KEY (fkUsuario) REFERENCES Usuario (idUsuario)
+  FOREIGN KEY (fkUsuario) REFERENCES usuario (idUsuario)
 );
 
 -- -----------------------------------------------------
 -- Table Contato
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS Contato ;
+DROP TABLE IF EXISTS contato ;
 
-CREATE TABLE IF NOT EXISTS Contato (
+CREATE TABLE IF NOT EXISTS contato (
   idContato INT PRIMARY KEY AUTO_INCREMENT,
   email VARCHAR(100) NULL,
   fkUsuario INT NOT NULL,
-  FOREIGN KEY (fkUsuario) REFERENCES Usuario (idUsuario)
+  FOREIGN KEY (fkUsuario) REFERENCES usuario (idUsuario)
 );
 
 -- -----------------------------------------------------
 -- Table Log
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS Log ;
+DROP TABLE IF EXISTS log ;
 
-CREATE TABLE IF NOT EXISTS Log (
+CREATE TABLE IF NOT EXISTS log (
   idLog INT PRIMARY KEY AUTO_INCREMENT,
   titulo VARCHAR(100) NULL,
   descricao TEXT NULL,
@@ -133,8 +133,8 @@ CREATE TABLE IF NOT EXISTS Log (
 -- -----------------------------------------------------
 -- Tabela FaleConosco
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS FaleConosco;
-CREATE TABLE IF NOT EXISTS FaleConosco (
+DROP TABLE IF EXISTS faleConosco;
+CREATE TABLE IF NOT EXISTS faleConosco (
     idFale INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(200),
     email VARCHAR(200),
@@ -146,7 +146,7 @@ CREATE TABLE IF NOT EXISTS FaleConosco (
 -- -----------------------------------------------------
 -- Tabela Planilha
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS Planilha (
+CREATE TABLE IF NOT EXISTS planilha (
     idPlanilha INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(200)
 );
